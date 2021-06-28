99eb418d1940d5096d7f2cc7ca9b82f9ca2ad41b
Ved Khandekar <vedk90@gmail.com>
2021-06-28 20:44:23 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# checkbox_example
+
+A small Flutter project to demonstrate the use of the `Checkbox` widget.
+
+## Getting Started
+
+Run the command below to clone this repo. Then open it in Android Studio.
+```
+git clone
+```
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Checkbox(
+          value: isChecked,
+          onChanged: (bool? value) {
+            setState(() {
+              // if you comment out the statement below, the checkbox will not
+              // change state
+              isChecked = value!;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
